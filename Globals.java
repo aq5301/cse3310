19c9bc8f2bd0d0078f26d9288313b02bb44657c1
aq5301 <ashleyq5301@att.net>
2016-04-27 18:17:32 -0500

    Added files via upload

--- Globals.java
+++ Globals.java
@@ -5,10 +5,19 @@ import android.app.Application;
 /**
  * Created by Ashley on 4/13/2016.
  */
+
+//Globals class is essential for loading up a previously started game. Must simply make a call for the information here
 public class Globals extends Application {
         private boolean volume = true;
         private boolean colorGraphic = true;
         private int difficulty = 1; //0 for easy
+        private int turn = 0, playerMoves = 0, playerHits = 0; //0 for player, 1 for AI
+        private Ship [] player_Ships;
+        private Ship [] AI_ships;
+
+        final private int [] SHIPLIST = {2, 3, 3, 4, 4}; //types of ships both player and CPU have
+
+
 
     public boolean getVolume(){
         return this.volume;
@@ -22,6 +31,30 @@ public class Globals extends Application {
         return this.difficulty;
     }
 
+    public int getTurn(){
+        return this.turn;
+    }
+
+    public int getPlayerMoves(){
+        return this.playerMoves;
+    }
+
+    public int getPlayerHits(){
+        return this.playerHits;
+    }
+
+    public int[] getSHIPLIST(){
+        return this.SHIPLIST;
+    }
+
+    public Ship[] getPlayerShips() {
+        return player_Ships;
+    }
+
+    public Ship[] getAIships() {
+        return AI_ships;
+    }
+
     public void setVolumeOn(boolean setV){
         this.volume = setV;
     }
@@ -33,6 +66,27 @@ public class Globals extends Application {
     public void setDifficulty(int setD){
         this.difficulty = setD;
     }
+
+    public void setTurn(int setT){
+        this.turn = setT;
+    }
+
+    public void setPlayerMoves(int setPM){
+        this.playerMoves = setPM;
+    }
+
+    public void setPlayerHits(int setPH){
+        this.playerMoves = setPH;
+    }
+
+    public void setPlayerShips(Ship[] playerShipsR) {
+        this.player_Ships = playerShipsR;
+    }
+
+    public void setAIships(Ship[] AI_ships) {
+        this.AI_ships = AI_ships;
+    }
+
 
 
 
