1306ef105a9476ab1e9ab918254b4f851adda302
aq5301 <ashleyq5301@att.net>
2016-04-29 16:46:35 -0500

    Update hw3.s

--- hw3.s
+++ hw3.s
@@ -59,7 +59,11 @@ read_arrays:
     
     B read_arrays
     
-
+_printf:
+    PUSH {LR}               @ store the return address
+    LDR R0, =printf_str     @ R0 contains formatted string address
+    BL printf               @ call printf
+    POP {PC}   
 
 print:
     PUSH {LR}
@@ -67,7 +71,25 @@ print:
     BL printf
     POP {PC}
 
-
+readloop:
+    CMP R0, #100            @ check to see if we are done iterating
+    BEQ exit          @ exit loop if done
+    LDR R1, =a              @ get address of a
+    LSL R2, R0, #2          @ multiply index*4 to get array offset
+    ADD R2, R1, R2          @ R2 now has the element address
+    LDR R1, [R2]            @ read the array at address 
+    PUSH {R0}               @ backup register before printf
+    PUSH {R1}               @ backup register before printf
+    PUSH {R2}               @ backup register before printf
+    MOV R2, R1              @ move array value to R2 for printf
+    MOV R1, R0              @ move array index to R1 for printf
+    BL  _printf             @ branch to print procedure with return
+    POP {R2}                @ restore register
+    POP {R1}                @ restore register
+    POP {R0}                @ restore register
+    ADD R0, R0, #1          @ increment index
+    B   readloop            @ branch to next loop iteration
+    
 generate:
     CMP R0, #20 
     BEQ sort_ascending
@@ -146,3 +168,4 @@ b:              .skip       80
 num_str:        .asciz      "%d"
 print_str:      .asciz      "array_a[%d] = %d, array_b = %d\n"
 prompt_str:     .ascii      "Enter an integer.\n"
+printf_str:     .asciz      "a[%d] = %d\n"
