96b659ae86dbfc961c36379c4cfe7128fbe7586e
aq5301 <ashleyq5301@att.net>
2016-04-30 17:10:01 -0500

    Update hw3.s

--- hw3.s
+++ hw3.s
@@ -8,7 +8,7 @@ main:
     BL scanint 
     MOV R5, R0
     MOV R0, #0
-              
+             
 generate:
     CMP R0, #20 
     BEQ next
@@ -91,10 +91,30 @@ aTob:
     
     ADD R0, R0, #1
     B aTob
-    
-    
 next3:
-    BL generate
+    MOV R0, #0
+    
+generate:
+    CMP R0, #20 
+    BEQ next4
+    LDR R1, =a
+    LSL R2, R0, #2
+    ADD R2, R1, R2
+
+    ADD R3, R5, R0
+    STR R3, [R2]          
+    ADD R4, R3, #1
+    NEG R3, R4
+
+    ADD R6, R0, #1
+    LSL R2, R6, #2
+    ADD R2, R1, R2
+    STR R3, [R2]
+    
+    ADD R0, R0, #2
+    B generate
+    
+next4:
     MOV R0, #0
     
 read_arrays:
