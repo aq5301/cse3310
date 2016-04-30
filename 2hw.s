2ad90009b098dede8957ad3fcbbc3543035449b8
aq5301 <ashleyq5301@att.net>
2016-04-30 15:01:25 -0500

    Update 2hw.s

--- 2hw.s
+++ 2hw.s
@@ -1,35 +1,25 @@
-    .global main
-    .func main
-   
-   
+.global main
+.func main
    
 main:
-    @BL prompt
-    @BL scanint
-    @MOV R5, R0
-    MOV R0, #0
-
-
-generate:
-    CMP R0, #20            @ check to see if we are done iterating
-    BEQ done          @ exit loop if done
+    MOV R0, #0              @ initialze index variable
+writeloop:
+    CMP R0, #20           @ check to see if we are done iterating
+    BEQ writedone           @ exit loop if done
     LDR R1, =a              @ get address of a
     LSL R2, R0, #2          @ multiply index*4 to get array offset
     ADD R2, R1, R2          @ R2 now has the element address
-    
     MOV R3, #3
     STR R3, [R2]            @ write the address of a[i] to a[i]
     MOV R4, #4
-    STR R4, [R2, #+1]
-    
+    STR R4, [R2,+#1]
     ADD R0, R0, #1          @ increment index
-    B generate
-
-done:
-    MOV R0, #0
+    B   writeloop           @ branch to next loop iteration
+writedone:
+    MOV R0, #0              @ initialze index variable
 readloop:
     CMP R0, #20            @ check to see if we are done iterating
-    BEQ exit          @ exit loop if done
+    BEQ readdone            @ exit loop if done
     LDR R1, =a              @ get address of a
     LSL R2, R0, #2          @ multiply index*4 to get array offset
     ADD R2, R1, R2          @ R2 now has the element address
@@ -44,24 +34,28 @@ readloop:
     POP {R1}                @ restore register
     POP {R0}                @ restore register
     ADD R0, R0, #1          @ increment index
-    B   readloop            @ branch to next loop iteration    
+    B   readloop            @ branch to next loop iteration
+readdone:
+    B _exit                 @ exit if done
     
-
-exit:   
-    MOV R7, #1         
-    SWI 0               
-    
+_exit:  
+    MOV R7, #4              @ write syscall, 4
+    MOV R0, #1              @ output stream to monitor, 1
+    MOV R2, #21             @ print string length
+    LDR R1, =exit_str       @ string at label exit_str:
+    SWI 0                   @ execute syscall
+    MOV R7, #1              @ terminate syscall, 1
+    SWI 0                   @ execute syscall
+       
 _printf:
     PUSH {LR}               @ store the return address
     LDR R0, =printf_str     @ R0 contains formatted string address
     BL printf               @ call printf
-    POP {PC}   
-
-
-
+    POP {PC}                @ restore the stack pointer and return
+   
 .data
 
-
 .balign 4
-a:              .skip       80
+a:              .skip       400
 printf_str:     .asciz      "a[%d] = %d\n"
+exit_str:       .ascii      "Terminating program.\n"
